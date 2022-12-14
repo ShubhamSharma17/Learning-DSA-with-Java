f9b4486d07e9dbd6090e9db23c7e3a44f3af3554
Shubham Sharma <shubhamsharmsa706568@gmail.com>
2022-12-14 13:19:24 +0530

    14-dec-22

---
+++ Leetcode_1207.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Leetcode_1207{
+    public static void main(String[] args) {
+        int [] array =  Lecture10.createArray();
+        System.out.println(uniqueOccurrences(array));
+        
+    }
+    public static boolean uniqueOccurrences(int[] arr) {
+        Arrays.sort(arr);
+        int i = 0;
+        int j = 0;
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        while(j < arr.length){
+            while( j < arr.length && arr[i] == arr[j]){
+                j++;
+            }
+            arrayList.add(j-i);
+            i = j;
+        }
+        for(int k = 0; k<arrayList.size(); k++)
+        {
+            System.out.print( " " + arrayList.get(k));
+        }
+        i = 0;
+        j = 1;
+        Collections.sort(arrayList);
+        while( j < arrayList.size()){
+            int num1 = arrayList.get(j);
+            int num2 = arrayList.get(i);
+            if( num1 <= num2){
+                return false;
+            }
+            j++;
+            i++;
+        }
+    return true;
+    }
+}
