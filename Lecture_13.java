9634cedef66ba5aec0c6b8e9d5176e1b2a885d46
Shubham Sharma <shubhamsharmsa706568@gmail.com>
2022-12-15 23:15:53 +0530

    15-dec-22

--- Lecture_13.java
+++ Lecture_13.java
@@ -2,8 +2,9 @@ import java.util.ArrayList;
 
 public class Lecture_13 {
     public static void main(String[] args) {
-        ArrayList<Integer> arrList = arrayList.createArrayList();
-        arrays.printArray(firstAndLastPosition(arrList, arrList.size(), 4));
+        // ArrayList<Integer> arrList = arrayList.createArrayList();
+        // arrays.printArray(firstAndLastPosition(arrList, arrList.size(), 4));
+        System.out.println(peakIndexInMountainArray(arrays.createArray()));
     }
 
     public static int[] firstAndLastPosition(ArrayList<Integer> arr, int size, int target) {
@@ -75,6 +76,28 @@ public class Lecture_13 {
             mid = start + (end - start)/2;
         }
         return right;
+    }
+
+
+    //852. Peak Index in a Mountain Array
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid = start + (end - start)/2;
+        // int peakIndex = 0
+        while(start <= end){
+            if((mid - 1) > 0 && arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]){
+                return mid;
+            }
+            if(arr[mid] < arr[mid + 1]){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+            mid = start + (end - start)/2;
+        }
+        return mid;
     }
     
 }
