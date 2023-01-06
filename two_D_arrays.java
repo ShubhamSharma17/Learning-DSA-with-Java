4ea2c9893b0c2dc615fbf281b3f96a002950fa24
Shubham Sharma <shubhamsharma706568@gmail.com>
2023-01-06 23:59:14 +0530

    6-jan-2023

---
+++ two_D_arrays.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class two_D_arrays {
+    public static int[][] create_2_DArray(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value for 2-D array");
+        System.out.print("Enter Number of Row :");
+        System.out.println();
+        int row = sc.nextInt();
+        System.out.print("Enter Number of Column :");
+        int column = sc.nextInt();
+        int [][]array = new int[row][column];
+        //taking input from user
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < column; j++)
+            {
+                int number = sc.nextInt();
+                array[i][j] = number;
+            }
+        }
+        return array;
+    }       
+        
+        //Print 2-D array
+        public static void print_2_D_Array(int[][] array){
+            int row = array.length;
+            int column = array[0].length;
+        for(int a = 0; a < row; a++)
+        {
+            for(int b = 0; b < column; b++)
+            {
+                System.out.print(array[a][b] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
